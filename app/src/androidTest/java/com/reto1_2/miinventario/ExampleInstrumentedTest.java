c9c1e21fdfda3158a9957064e7461b7881b277a9
nian8203 <nelsonrodriguez@misena.edu.co>
2021-12-07 17:12:52 -0500

    creacion app

---
+++ app/src/androidTest/java/com/reto1_2/miinventario/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package com.reto1_2.miinventario;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.reto1_2.miinventario", appContext.getPackageName());
+    }
+}
